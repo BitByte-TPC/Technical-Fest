1b43a0dbd92f9642ca2ac58933cfc7292e8eb73f
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-09-02 08:38:53 +0530

    Issue #3 solved

--- app/src/main/java/com/priyanshnama/technical_fest/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/priyanshnama/technical_fest/ui/profile/ProfileFragment.java
@@ -3,7 +3,6 @@ package com.priyanshnama.technical_fest.ui.profile;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,14 +14,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.priyanshnama.technical_fest.R;
 import com.priyanshnama.technical_fest.UpgradePassActivity;
 import com.squareup.picasso.Picasso;
-
-import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
@@ -71,7 +67,7 @@ public class ProfileFragment extends Fragment {
 
         name.setText(userInfo.getString("name","Name"));
         email.setText(userInfo.getString("email","Email"));
-        //passView.setText(userInfo.getInt("pass",0));
+        passView.setText(String.valueOf(userInfo.getInt("pass",0)));
         festId.setText(userInfo.getString("festId","festId"));
         Picasso.get().load(userInfo.getString("photoUrl","null")).into(profilePic);
     }
