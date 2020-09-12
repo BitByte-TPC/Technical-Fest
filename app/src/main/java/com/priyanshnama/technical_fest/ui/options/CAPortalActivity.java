c9d2d60b10e0da8e5fe888086a6c9b7513f68538
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-09-12 17:43:59 +0530

    Options Added. minor changes in event_info.xml options_menu.xml linked to fragment_profile.xml through a ic_menu.xml and configured in ProfileFragment.java. All options linked to Respective Activities. Supportive dimens and strings added in dimens.xml and strings.xml.

---
+++ app/src/main/java/com/priyanshnama/technical_fest/ui/options/CAPortalActivity.java
@@ -0,0 +1,16 @@
+package com.priyanshnama.technical_fest.ui.options;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.priyanshnama.technical_fest.R;
+
+public class CAPortalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ca_portal);
+    }
+}
--- app/src/main/java/com/priyanshnama/technical_fest/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/priyanshnama/technical_fest/ui/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.priyanshnama.technical_fest.ui.profile;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,11 +8,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -21,7 +22,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.priyanshnama.technical_fest.MainActivity;
 import com.priyanshnama.technical_fest.R;
 import com.priyanshnama.technical_fest.UpgradePassActivity;
+import com.priyanshnama.technical_fest.ui.options.AboutActivity;
+import com.priyanshnama.technical_fest.ui.options.CAPortalActivity;
+import com.priyanshnama.technical_fest.ui.options.ContactUsActivity;
+import com.priyanshnama.technical_fest.ui.options.TeamActivity;
 import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
@@ -29,6 +36,7 @@ public class ProfileFragment extends Fragment {
     private TextView name, email, festId, passView;
     private ImageView profilePic;
     private Integer pass;
+    private Dialog panel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
@@ -40,16 +48,27 @@ public class ProfileFragment extends Fragment {
         profilePic = root.findViewById(R.id.profile_image);
         passView = root.findViewById(R.id.pass);
 
-        Button upgrade = root.findViewById(R.id.upgrade);
-        upgrade.setOnClickListener(v -> startActivity(new Intent(getContext(), UpgradePassActivity.class)));
-
-        Button signOut = root.findViewById(R.id.signOut);
-        signOut.setOnClickListener(this::signOut);
+        root.findViewById(R.id.upgrade).setOnClickListener(v -> startActivity(new Intent(getContext(), UpgradePassActivity.class)));
+        root.findViewById(R.id.options_menu).setOnClickListener(v-> panel.show());
 
         SharedPreferences userInfo =  this.requireActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         profileViewModel.sendPrefrence(userInfo);
         setData();
+        prepareOptions();
         return root;
+    }
+
+    private void prepareOptions() {
+        panel = new Dialog(requireContext());
+        panel.setContentView(R.layout.options_menu);
+        panel.setCancelable(true);
+        Objects.requireNonNull(panel.getWindow()).setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+
+        panel.findViewById(R.id.contact).setOnClickListener(v-> startActivity(new Intent(requireContext(), ContactUsActivity.class)));
+        panel.findViewById(R.id.about).setOnClickListener(v-> startActivity(new Intent(requireContext(), AboutActivity.class)));
+        panel.findViewById(R.id.ca).setOnClickListener(v-> startActivity(new Intent(requireContext(), CAPortalActivity.class)));
+        panel.findViewById(R.id.team).setOnClickListener(v-> startActivity(new Intent(requireContext(), TeamActivity.class)));
+        panel.findViewById(R.id.signOut).setOnClickListener(this::signOut);
     }
 
     private void setData() {
